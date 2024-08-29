f307c6053520578c15014526d22df897461e19e5
Eduardo Salinas <eduardoluis93@gmail.com>
2024-08-29 14:38:28 +0100

    Started Episode 2 from Brackeys Tutorial
    Started doing the first half of brackey's tutorial. The camera works well, and I can even rotate the camera.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 /* This script will let the player move.
 
-Source of most of this code: Brackeys from https://youtu.be/S2mK6KFdv0I?si=j-YFg86whpVS1Py0 .
+Source of most of this code: Brackeys from https://youtu.be/S2mK6KFdv0I?si=j-YFg86whpVS1Py0 , https://youtu.be/9tePzyL6dgc?si=4DXgIfg0ETg8TWuu 
  */
 
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
+
+    public Interactable focus;
 
     public LayerMask movementMask;
 
@@ -26,17 +28,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // If we press left mouse
         if (Input.GetMouseButtonDown(0))
+        {
+            // We create a ray
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If the ray hits
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
+            }
+        }
+
+        // If we press the right mouse
+        if (Input.GetMouseButtonDown(1))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            if (Physics.Raycast(ray, out hit, 100))
             {
-                // Debug.Log("We hit " + hit.collider.name + " " + hit.point);
-                
-                motor.MoveToPoint(hit.point);
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+            
+                if (interactable != null)
+                {
+
+                    SetFocus(interactable);
+                }
             }
         }
     }
+
+
+    void SetFocus(Interactable newFocus)
+    {
+        focus = newFocus;
+        motor.FollowTarget(newFocus);
+    }
+
+    void RemoveFocus()
+    {
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+/* This script will let the player move (as in moving from 1 point to the other).
+
+Source of most of this code: Brackeys from https://youtu.be/9tePzyL6dgc?si=rswsagD6YwbvYRs2 .
+*/
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour
 {
 
+    Transform target;
     NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -15,8 +21,27 @@ public class PlayerMotor : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
     }
 
+    void Update () 
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+
+        }
+    }
+
     public void MoveToPoint(Vector3 point)
     {
         agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        target = newTarget.transform;
+    }
+
+    public void StopFollowingTarget()
+    {
+        target = null;
     }
 }
