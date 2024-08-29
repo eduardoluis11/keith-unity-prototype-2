4276f46e98f3cd3f8404fceb526ef778a148fae3
Eduardo Salinas <eduardoluis93@gmail.com>
2024-08-29 11:55:27 +0100

    BUGFIX: I fixed a bug in which the Player would Rotate, but it wouldn't move (translate) when I clicked anywhere on the ground.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* This script will let the player move.
+
+Source of most of this code: Brackeys from https://youtu.be/S2mK6KFdv0I?si=j-YFg86whpVS1Py0 .
+ */
+
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
 
+    public LayerMask movementMask;
+
     Camera cam;
+    PlayerMotor motor;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
     }
 
     // Update is called once per frame
@@ -21,10 +31,11 @@ public class PlayerController : MonoBehaviour
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
-                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
-
+                // Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                
+                motor.MoveToPoint(hit.point);
             }
         }
     }
