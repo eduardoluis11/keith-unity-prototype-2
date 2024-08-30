bb20383265290b617ed2d097529b1588a50a7e70
Eduardo Salinas <eduardoluis93@gmail.com>
2024-08-30 11:12:50 +0100

    Finished Brackey's Episode 2
    I finished Episode 2 of Brackey's 'How to make an RPG in Unity' tutorial without the 'Vector3 is 0' warning message bug.

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -9,20 +9,28 @@ public class Interactable : MonoBehaviour
 {
 
     public float radius = 3f;
+    public Transform interactionTransform;
 
     bool isFocus = false;
     Transform player;
 
     bool hasInteracted = false;
 
+    public virtual void Interact ()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
     void Update ()
     {
         if (isFocus && !hasInteracted)
         {
-            float distance = Vector3.Distance(player.position, transform.position);
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
             if (distance <= radius)
             {
-                Debug.Log("INTERACTING");
+                // Debug.Log("INTERACTING");
+                Interact();
                 hasInteracted = true;
             }
         }
@@ -45,6 +53,6 @@ public class Interactable : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
     }
 }
