faefb030ee31870cb1514330234dbe96cf266d33
Eduardo Salinas <eduardoluis93@gmail.com>
2024-09-03 14:48:40 +0100

    Tutorial: I created a New Scene to Test using a Render Texture from a Code Monkey Tutorial.

---
+++ Assets/Scripts/Items/ItemSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This wil help me inspect an item in a 3D viewer (like in Skyrim or in Fallout New Vegas.)
+
+Source of most of this code: Code Monkey from https://www.youtube.com/watch?v=tJ_ycboPFmY 
+*/
+
+public class ItemSO : ScriptableObject
+{
+
+    public Sprite sprite;
+    public Transform prefab;
+
+}
