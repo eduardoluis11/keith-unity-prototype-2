85e1fd7d49a8de3426f8056abf1a3a16c064bdcb
Eduardo Salinas <eduardoluis93@gmail.com>
2024-09-30 22:23:19 +0100

    DEBUGGING: I'm testing a Quiz Game in a Test Scene. I will later use that Quiz Game, and replace the Puzzle Wall Puzzle with the Quiz game to determine the order in which you need to select the books.

---
+++ Assets/Scripts/SubmitButtonHandler.cs
@@ -0,0 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class SubmitButtonHandler : MonoBehaviour
+// {
+//     public ItemSlotManager itemSlotManager;
+
+//     private void Start()
+//     {
+//         // Get the Button component and add a listener to the click event
+//         GetComponent<Button>().onClick.AddListener(OnSubmitButtonClicked);
+//     }
+
+//     private void OnSubmitButtonClicked()
+//     {
+//         if (itemSlotManager != null)
+//         {
+//             itemSlotManager.OnSubmitButtonClicked();
+//         }
+//     }
+
+// }
